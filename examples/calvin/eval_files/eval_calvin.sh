32a31861f1f86730b8c13b5a6442b06daefcccff
wangxuesong5 <sx_wangxuesong5@intern.lixiang.com>
2026-03-09 21:03:20 +0800

    fix eval calvin

--- examples/calvin/eval_files/eval_calvin.sh
+++ examples/calvin/eval_files/eval_calvin.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+## pip install tyro websockets msgpack rich accelerate moviepy==1.0.3
 
 ###########################################################################################
 # === Please modify the following paths according to your environment ===
 export PYTHONPATH=$(pwd):${PYTHONPATH} # let Calvin client find websocket tools from main repo
-export calvin_python=/path/to/your/conda/envs/calvin/bin/python
 
+gpu_id=2
 host="127.0.0.1"
-base_port=5694
+base_port=569$gpu_id
 unnorm_key="franka"
-your_ckpt=results/Checkpoints/0123_starvla_qwen3_calvin_task_D_D/checkpoints/steps_30000_pytorch_model.pt
+your_ckpt=results/Checkpoints/calvin_task_D_D_qwen3gr00t/checkpoints/steps_8000_pytorch_model.pt
 
 folder_name=$(echo "$your_ckpt" | awk -F'/' '{print $(NF-2)"_"$(NF-1)"_"$NF}')
 # === End of environment variable configuration ===
@@ -17,10 +18,15 @@ folder_name=$(echo "$your_ckpt" | awk -F'/' '{print $(NF-2)"_"$(NF-1)"_"$NF}')
 LOG_DIR="logs/$(date +"%Y%m%d_%H%M%S")"
 mkdir -p ${LOG_DIR}
 
-${calvin_python} ./examples/calvin/eval_files/eval_calvin.py \
+export CUDA_VISIBLE_DEVICES=2
+torchrun --nproc_per_node=1 --master_port=259$gpu_id \
+ ./examples/calvin/eval_files/eval_calvin.py \
     --args.pretrained-path ${your_ckpt} \
     --args.unnorm-key ${unnorm_key} \
     --args.host "$host" \
     --args.port $base_port \
-    --args.dataset_path /path/to/calvin/task_D_D/ \
-    --args.num_sequences 1000
+    --args.dataset_path /mnt/volumes/base-3da-ali-sh-mix/xswang/object/datasets/src/calvin/task_ABC_D \
+    --args.calvin_config_path /mnt/volumes/base-3da-ali-sh-mix/xswang/object/reference/calvin/calvin_models/conf \
+    --args.eval_sequences_path examples/calvin/eval_files/eval_sequences.json \
+    --args.num_sequences 1000 \
+    2>&1 | tee "${LOG_DIR}/eval_calvin.log"
--- examples/calvin/eval_files/run_policy_server.sh
+++ examples/calvin/eval_files/run_policy_server.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd):${PYTHONPATH} # let LIBERO find the websocket tools from main repo
-export star_vla_python=/mnt/data/miniconda3/envs/starvla/bin/python
-your_ckpt=results/Checkpoints/0118_starvla_qwenpi_calvin_task_D_D/checkpoints/steps_30000_pytorch_model.pt
-gpu_id=0
-port=5694
+export HF_HUB_ETAG_TIMEOUT=86400
+export HF_HUB_DOWNLOAD_TIMEOUT=86400
+export HF_ENDPOINT=https://artifactory-cloud.chehejia.com/artifactory/api/huggingfaceml/huggingface-remote
+export WANDB_MODE=offline
+
+your_ckpt=results/Checkpoints/calvin_task_D_D_qwen3gr00t/checkpoints/steps_8000_pytorch_model.pt
+gpu_id=1
+port=5692
 ################# star Policy Server ######################
 
 # export DEBUG=true
-CUDA_VISIBLE_DEVICES=$gpu_id ${star_vla_python} deployment/model_server/server_policy.py \
+CUDA_VISIBLE_DEVICES=$gpu_id torchrun --nproc_per_node=1 --master_port 10192 \
+    deployment/model_server/server_policy.py \
     --ckpt_path ${your_ckpt} \
     --port ${port} \
     --use_bf16
