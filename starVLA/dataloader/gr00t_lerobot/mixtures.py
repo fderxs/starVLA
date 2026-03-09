b9ca6ffc7184b94e16e9ba3aabaa3519ca62370e
wangxuesong5 <sx_wangxuesong5@intern.lixiang.com>
2026-03-09 21:17:43 +0800

    add calvin & bflc in mixtures

--- starVLA/dataloader/gr00t_lerobot/mixtures.py
+++ starVLA/dataloader/gr00t_lerobot/mixtures.py
@@ -34,6 +34,25 @@ DATASET_NAMED_MIXTURES = {
         ("bridge_orig_1.0.0_lerobot", 1.0, "oxe_bridge"),
         ("fractal20220817_data_0.1.0_lerobot", 1.0, "oxe_rt1"),
     ],
+    "calvin_task_ABC_D": [
+        ("calvin_abc_d_1.0.0_lerobot", 1.0, "libero_franka"),
+    ],
+    "calvin_task_D_D": [
+        ("calvin_d_d_1.0.0_lerobot", 1.0, "libero_franka"),
+    ],
+    "bridge_fractal_libero_calvin": [
+        # Bridge dataset
+        ("bridge_orig_1.0.0_lerobot", 1.0, "oxe_bridge"),
+        # Fractal (RT-1) dataset
+        ("fractal20220817_data_0.1.0_lerobot", 1.0, "oxe_rt1"),
+        # Libero datasets
+        ("libero_object_no_noops_1.0.0_lerobot", 1.0, "libero_franka"),
+        ("libero_goal_no_noops_1.0.0_lerobot", 1.0, "libero_franka"),
+        ("libero_spatial_no_noops_1.0.0_lerobot", 1.0, "libero_franka"),
+        ("libero_10_no_noops_1.0.0_lerobot", 1.0, "libero_franka"),
+        # Calvin datasets - ABC and D scenarios
+        ("calvin_abc_d_1.0.0_lerobot", 1.0, "libero_franka"),
+    ],
 
     "demo_sim_pick_place": [
         ("sim_pick_place", 1.0, "demo_sim_franka_delta_joints"),
