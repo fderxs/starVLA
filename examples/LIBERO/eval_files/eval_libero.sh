9c1de8e338d73c72ac6ab70c91cafcaa9aa83c13
wangxuesong5 <sx_wangxuesong5@intern.lixiang.com>
2026-03-09 21:04:36 +0800

    fix eval libero

--- examples/LIBERO/eval_files/eval_libero.sh
+++ examples/LIBERO/eval_files/eval_libero.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-
-cd /mnt/petrelfs/yejinhui/Projects/starVLA
-conda activate starVLA
+## pip install tyro websockets msgpack rich accelerate
 
 ###########################################################################################
+gpu_id=1
+
 # === Please modify the following paths according to your environment ===
-export LIBERO_HOME=/mnt/petrelfs/share/yejinhui/Projects/LIBERO
+export LIBERO_HOME=/mnt/volumes/base-3da-ali-sh-mix/xswang/object/reference/LIBERO
 export LIBERO_CONFIG_PATH=${LIBERO_HOME}/libero
-export LIBERO_Python=/mnt/petrelfs/share/yejinhui/Envs/miniconda3/envs/lerobot/bin/python
 
 export PYTHONPATH=$PYTHONPATH:${LIBERO_HOME} # let eval_libero find the LIBERO tools
 export PYTHONPATH=$(pwd):${PYTHONPATH} # let LIBERO find the websocket tools from main repo
 
+export CUDA_VISIBLE_DEVICES=2
+
 
 host="127.0.0.1"
-base_port=5694
+base_port=569$gpu_id
 unnorm_key="franka"
-your_ckpt=./results/Checkpoints/1208_libero_all_QwenPI_qwen3/checkpoints/steps_50000_pytorch_model.pt
-export DEBUG=true
+your_ckpt=/mnt/volumes/base-3da-ali-sh-mix/xswang/object/pretrained/StarVLA/StarVLA__Qwen2_5-VL-GR00T-LIBERO-4in1/25-10-28-1624/checkpoints/steps_30000_pytorch_model.pt
 
 folder_name=$(echo "$your_ckpt" | awk -F'/' '{print $(NF-2)"_"$(NF-1)"_"$NF}')
 # === End of environment variable configuration ===
 ###########################################################################################
 
-LOG_DIR="logs/$(date +"%Y%m%d_%H%M%S")"
+LOG_DIR="logs/$(date +"%Y%m%d_%H%M%S")_libero"
 mkdir -p ${LOG_DIR}
 
 
@@ -32,10 +32,12 @@ num_trials_per_task=50
 video_out_path="results/${task_suite_name}/${folder_name}"
 
 
-${LIBERO_Python} ./examples/LIBERO/eval_files/eval_libero.py \
+torchrun --nproc_per_node=1 --master_port=2591$gpu_id \
+    ./examples/LIBERO/eval_files/eval_libero.py \
     --args.pretrained-path ${your_ckpt} \
     --args.host "$host" \
     --args.port $base_port \
     --args.task-suite-name "$task_suite_name" \
     --args.num-trials-per-task "$num_trials_per_task" \
-    --args.video-out-path "$video_out_path"
+    --args.video-out-path "$video_out_path" \
+    2>&1 | tee "${LOG_DIR}/eval.log"
--- examples/LIBERO/eval_files/run_policy_server.sh
+++ examples/LIBERO/eval_files/run_policy_server.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd):${PYTHONPATH} # let LIBERO find the websocket tools from main repo
-export star_vla_python=/mnt/petrelfs/share/yejinhui/Envs/miniconda3/envs/starVLA/bin/python
-your_ckpt=results/Checkpoints/1208_libero_all_QwenPI_qwen3/checkpoints/steps_50000_pytorch_model.pt
-gpu_id=7
-port=5694
+export HF_HUB_ETAG_TIMEOUT=86400
+export HF_HUB_DOWNLOAD_TIMEOUT=86400
+export HF_ENDPOINT=https://artifactory-cloud.chehejia.com/artifactory/api/huggingfaceml/huggingface-remote
+export WANDB_MODE=offline
+
+your_ckpt=/mnt/volumes/base-3da-ali-sh-mix/xswang/object/pretrained/StarVLA/StarVLA__Qwen2_5-VL-GR00T-LIBERO-4in1/25-10-28-1624/checkpoints/steps_30000_pytorch_model.pt
+gpu_id=1
+port=569$gpu_id
 ################# star Policy Server ######################
 
 # export DEBUG=true
-CUDA_VISIBLE_DEVICES=$gpu_id ${star_vla_python} deployment/model_server/server_policy.py \
+CUDA_VISIBLE_DEVICES=$gpu_id torchrun --nproc_per_node=1 --master_port 1019$gpu_id \
+    deployment/model_server/server_policy.py \
     --ckpt_path ${your_ckpt} \
     --port ${port} \
     --use_bf16
