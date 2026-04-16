56e2008d463d470edd7884664ced18f40ed09d55
wangxuesong5 <sx_wangxuesong5@intern.lixiang.com>
2026-04-16 15:54:55 +0800

    add total size google robot evaluation

--- examples/SimplerEnv/eval_files/run_eval_google_robot.sh
+++ examples/SimplerEnv/eval_files/run_eval_google_robot.sh
@@ -32,11 +32,18 @@ declare -a suites=(
     put_in_drawer_visual_matching
 )
 
-for ((run_idx=start_run_id; run_idx<=start_run_id+test_num-1; run_idx++)); do
-    for suite in "${suites[@]}"; do
-        LOG_DIR="logs/google_robot/${folder_name}/${suite}"
-        mkdir -p ${LOG_DIR}
-        echo "Running suite: ${suite}"
-        bash examples/SimplerEnv/eval_files/auto_eval_scripts/sub_eval/star_${suite}.sh ${ckpt_path} ${gpu_id} ${VIDEO_DIR} 2>&1 | tee "${LOG_DIR}/eval_${run_idx}.log"
-    done
-done
+# # sub evaluation for each suite
+# for ((run_idx=start_run_id; run_idx<=start_run_id+test_num-1; run_idx++)); do
+#     for suite in "${suites[@]}"; do
+#         LOG_DIR="logs/google_robot/${folder_name}/${suite}"
+#         mkdir -p ${LOG_DIR}
+#         echo "Running suite: ${suite}"
+#         bash examples/SimplerEnv/eval_files/auto_eval_scripts/sub_eval/star_${suite}.sh ${ckpt_path} ${gpu_id} ${VIDEO_DIR} 2>&1 | tee "${LOG_DIR}/eval_${run_idx}.log"
+#     done
+# done
+
+# total evaluation
+LOG_DIR="logs/google_robot/${folder_name}/${suites[$gpu_id]}"
+mkdir -p ${LOG_DIR}
+echo "Running suite: ${suites[$gpu_id]}"
+bash examples/SimplerEnv/eval_files/auto_eval_scripts/star_${suites[$gpu_id]}.sh ${ckpt_path} ${gpu_id} ${VIDEO_DIR} 2>&1 | tee "${LOG_DIR}/eval_full.log"
